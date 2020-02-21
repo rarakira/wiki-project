375f6fbf32ab4bcadf5cd435c3d98f74ed62e94f
Kira <rarakira@gmail.com>
2020-02-21 12:42:52 +0300

    Add get, put, patch, delete methods to single posts

--- app.js
+++ app.js
@@ -67,6 +67,57 @@ app.route("/articles")
     });
   });
 
+app.route("/articles/:singlePost")
+  .get((req, res) => {
+    Article.findOne({title: req.params.singlePost}, (err, post) => {
+
+      if (!err) {
+        if (post) {
+          res.send(post);
+        } else {
+          res.send("No articles matching this title were found.");
+        }
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .put((req, res) => {
+    Article.update(
+      {title: req.params.singlePost},
+      {title: req.body.title, content: req.body.content},
+      {overwrite: true},
+      err => {
+      if (!err) {
+        res.send("Successfully changed the article!");
+      } else {
+        res.send(err);
+      }
+    })
+  })
+  .patch((req, res) => {
+    Article.update(
+      {title: req.params.singlePost},
+      {$set: req.body},
+      err => {
+      if (!err) {
+        res.send("Successfully updated the article!");
+      } else {
+        res.send(err);
+      }
+    })
+  })
+  .delete((req, res) => {
+    Article.deleteOne({title: req.params.singlePost}, err => {
+      if (!err) {
+        res.send("Successfully deleted the article.");
+      } else {
+        res.send(err);
+      }
+    })
+  });
+
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
